edcb87d91442c58dde328ee19d7813b4d68b5516
Dozier, Patrick David <pdozier3@gatech.edu>
2016-04-11 22:48:01 -0400

    modified:   Pll/Pll_0002.v 	modified:   Project.qsf 	modified:   Project.sof 	modified:   Timer.v 	modified:   PLLJ_PLLSPE_INFO.txt 	modified:   Project.asm.rpt

--- Timer.v
+++ Timer.v
@@ -1,45 +1,70 @@
 module Timer (
 	input wire [31:0] ABUS,
 	inout wire [31:0] DBUS,
-	input wire WE,CLK,LOCK,INIT,CLOCK,
+	input wire WE,CLK,LOCK,INIT,
 	output wire INTR );
 	
-	parameter BITS;
-	parameter BASE;
+	parameter BITS = 32;
+	parameter TCNTADDR = 32'hFFFFF100;
+	parameter TLIMADDR = 32'hFFFFF104;
+	parameter TCTLADDR = 32'hFFFFF100;
 	parameter MILLS;
 	
-	wire selCtl = (ABUS == BASE);
-	wire wrCtl = selCtl && WE;
+	wire selCnt = (ABUS == TCNTADDR);
+	wire selLim = (ABUS == TLIMADDR);
+	wire selCtl = (ABUS == TCTLADDR);
+	wire wrCnt = selCnt && WE;
+	wire rdCnt = selCnt && (!WE);
+	wire wrLim = selLim && WE;
+	wire rdLim = selLim && (!WE);
+	wire wrCtl = selCtl && WE && !(DBUS[1] | DBUS[0]);
 	wire rdCtl = selCtl && (!WE);
 	
 	//Timer limit register
-	reg [4:0] TLIM=0;
+	reg [31:0] TCNT=0;
 	//Timer control register
 	reg [4:0] TCTL=0;
 	//Timer count register
-	reg [4:0] TCNT=0;
+	reg [31:0] TLIM=0;
+	reg [23:0] count;
 	//will change the size of TCTL as needed
 	//count of clock cycles to track 1 millisecond
-	reg [23:0]count;
-	wire checkMilsec = (count==MILLS);
-	wire inBounds = (TCNT <= (TLIM-4'd1));
+	wire checkMilsec = (count==MILLS-1);
+	wire limEn = (TLIM!=0);
+	wire limHit = TCNT==TLIM-1;
 	
 	//basically, if it hasn't been a millisecond, feed back in same values. If TCNT is within
 	//the TLIM-1 range, increment it. If TLIM is nonzero and TCNT is out of bounds, the read
 	//bit is set and TCNT goes back to 0. If the read bit was already set, the overflow bit
 	//gets set.
-	always @(posedge CLOCK) begin
+	always @(posedge CLK) begin
 		count<=checkMilsec?24'd0:count+24'd1;
-		if(TLIM!=0) begin
-			TCNT<=checkMilsec?inBounds&(TCNT+4'd1):TCNT;
-			TCTL<=checkMilsec?{TCTL[0]&!inBounds,!inBounds}:TCTL;
-		end else
-			TCNT<=checkMilsec?TCNT+4'd1:TCNT;
+		TCNT<=checkMilsec?(TCNT+4'd1):{(BITS){1'bX}};
+		if (limHit & limEn) begin
+			TCNT<=0;
+			if (TCTL[0]) begin
+				TCTL[1]<=1;
+			end else begin
+				TCTL[0]<=1;
+			end
+		end
+		if (wrCnt) begin 
+			TCNT<=DBUS;
+		end
+		if (wrLim) begin
+			TLIM<=DBUS;
+		end
+		if (wrCtl) begin
+			TCTL<=DBUS;
+		end
 	end
 	
-	assign DBUS =	rdCtl?{{28{1'b0}},TCTL}:
-						wrCtl?{{24{1'b0}},TCNT}:
-						{32{1'bZ}};
+	wire [31:0] DBUS_CNT = rdCnt?TCNT:{(BITS){1'bX}};
+	wire [31:0] DBUS_LIM = rdLim?TLIM:{(BITS){1'bX}};
+	wire [31:0] DBUS_CTL = rdCtl?TCTL:{(BITS){1'bX}};
+	
+	assign DBUS=(DBUS_CNT | DBUS_LIM | DBUS_CTL);
+	
 	//keep interrupt request 0 for now
 	assign INTR = 0;
 endmodule
